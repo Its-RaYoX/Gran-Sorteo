f5d45fc2f456d603ca74ca3564db8a38a265a0b5
Its-RaYoX <100781298+Its-RaYoX@users.noreply.github.com>
2024-10-09 20:30:39 +0200

    GranSorteo.java

--- GranSorteo.java
+++ GranSorteo.java
@@ -1,15 +1,7 @@
 //Importaciones necesarias para la ejecución del programa
-
 import java.util.Random;
 import java.util.Scanner;
 import java.util.InputMismatchException;
-
-/* RESUMEN DEL PROGRAMA
-
-El programa de sorteo permite a los usuarios participar ingresando un número de boletos (de 1 a 5) y sus números dentro de un rango dado. 
-Al aceptar para jugar, el usuario elige cuántos boletos desea y el programa genera un número ganador aleatorio. 
-Luego el usuario ingresa sus números y el programa busca si alguno coincide con el número ganador, informando si ha ganado o no. 
-Y al final se le pregunta si desea repetir el sorteo, continuando o finalizando según la respuesta que de el usuario. */
 
 public class GranSorteo {
     public static void main(String[] args) {
